9b243e31d14df7099c363c908e7b29de8b79de2c
Zeynep Ezgi Yalçınkaya <61873515+Zezgims@users.noreply.github.com>
2022-02-15 16:00:27 +0300

    Create image prossessing

---
+++ ConvertImagetoGray.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+image = cv2.imread("image.jpg")     
+grey = cv2.imread("image.jpg", 0)      
+ret, black = cv2.threshold(grey, 100, 255, cv2.THRESH_BINARY)        
+
+cv2.imshow("Image", image)
+cv2.imshow("Grey Image", grey)
+cv2.imshow("Black and White Image", black)
+
+
+#Converting image to gray with non-ready function
+
+x,y = image.shape[:2]       
+grey2 = np.zeros((x,y), "uint8")   
+
+for i in range(0,x):
+    for j in range(0,y):
+        sum = 0
+        for k in range(0,3):
+            sum += image[i][j][k]
+
+        pixel = sum/3   
+        grey2[i][j] = pixel
+
+cv2.imshow("Grey Image2", grey2)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
