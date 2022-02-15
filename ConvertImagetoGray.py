664a7d197bc999ff937d7a764059d2050d5ae34c
Zeynep Ezgi Yalçınkaya <61873515+Zezgims@users.noreply.github.com>
2022-02-15 16:20:40 +0300

    Create image processing code

--- ConvertImagetoGray.py
+++ ConvertImagetoGray.py
@@ -8,7 +8,7 @@ grey = cv2.imread("image.jpg", 0)
 cv2.imshow("Image", image)
 cv2.imshow("Grey Image", grey)
 
-#Converting image to gray with non-ready function
+#Converting image to gray with non-ready function.
 
 x,y = image.shape[:2]       
 grey2 = np.zeros((x,y), "uint8")   
