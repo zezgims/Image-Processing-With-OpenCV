1e7c4ba758e510228303b4418477f5e2e482c464
Zeynep Ezgi Yalçınkaya <61873515+Zezgims@users.noreply.github.com>
2022-02-15 16:46:50 +0300

    Create image processing code

--- ArithmeticOperationsonImage2.py
+++ ArithmeticOperationsonImage2.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 image = cv2.imread("image.jpg")
-image2 = cv2.imread("image2.jpg")
+image2 = cv2.imread("logo.jpg")
 
 sum = cv2.add(image, image2)                            #Adds pictures together equally.
 sum2 = cv2.addWeighted(image, 0.2, image2, 0.8, 0)      #Adds pictures together according to conditions.
