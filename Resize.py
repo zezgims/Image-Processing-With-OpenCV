958ddc429f20a9b193cbe42b9b43b11d0c547cb7
Zeynep Ezgi Yalçınkaya <61873515+Zezgims@users.noreply.github.com>
2022-02-15 16:44:42 +0300

    Create image processing code

--- Resize.py
+++ Resize.py
@@ -6,9 +6,9 @@ image = cv2.imread("image.jpg")
 up = cv2.pyrUp(image) 
 down = cv2.pyrDown(image)
 
-cv2.imshow("Orijinal",image)
-cv2.imshow("Up",up)
-cv2.imshow("Down",down)
+cv2.imshow("Orijinal", image)
+cv2.imshow("Up", up)
+cv2.imshow("Down", down)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
